5628c8a76f31fc5121485ec798ba1dd0dd6f459e
tracy <424133879@qq.com>
2024-01-04 23:42:52 +0800

    first b

---
+++ src/第一章/First.java
@@ -0,0 +1,20 @@
+public class First {
+    // test forasd as 
+
+     public static void main(String[] args) {
+        System.out.println("First Shot");
+     }
+
+     /**
+      * asd asd sa das
+      sad asd asd 
+      asd asd a das
+      */
+
+
+      /*
+       * asd asd ad 
+       * asd sad as
+       * asd as das asd 
+       */
+}
